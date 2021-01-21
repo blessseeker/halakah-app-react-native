480cfb684b128af3aebe00ce5b8786d0b841a0c5
Kamaludin Khoir <khoirkamaludin@gmail.com>
2021-01-22 02:34:15 +0700

    initial commit

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Button, ThemeProvider} from 'react-native-elements';
+
+const App = () => {
+  return (
+    <ThemeProvider>
+      <Button title="Hey!" />
+    </ThemeProvider>
+  );
+};
+
+export default App;
